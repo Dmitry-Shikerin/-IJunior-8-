bcf319417e6f4d2a2661d23a2e4df4e70907e481
Dmitry-Shikerin <130661730+Dmitry-Shikerin@users.noreply.github.com>
2023-08-06 19:42:23 +0300

    на отправку

--- Гладиаторские бои/Program.cs
+++ Гладиаторские бои/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Threading;
+using System.Collections.Generic;
 
 namespace Гладиаторские_бои
 {
@@ -17,14 +17,16 @@ namespace Гладиаторские_бои
     {
         protected int Armor;
 
-        public Fighter(int health, int armor, int damage)         
-        {                                                         
+        public Fighter(string name, int health, int armor, int damage)         
+        {  
+            Name = name;
             Health = health;                                      
             Armor = armor;
             Damage = damage;
             IsAlive = true;
         }
 
+        public string Name { get; protected set; }
         public int Health { get; protected set; }
         public bool IsAlive { get; private set; }                 
         public int Damage { get; private set; }                   
@@ -66,7 +68,7 @@ namespace Гладиаторские_бои
     {
         public int _rage = 0;                                           
 
-        public Warriour() : base(150, 12, 30) { }
+        public Warriour() : base("Воин", 150, 12, 30) { }
 
         public override int DealDamage()
         {
@@ -98,7 +100,7 @@ namespace Гладиаторские_бои
 
     class DeathKnight : Fighter
     {
-        public DeathKnight() : base(150, 15, 25) { }
+        public DeathKnight() : base("Рыцарь смерти", 150, 15, 25) { }
 
         public override void TakeDamage(int damage)
         {
@@ -128,7 +130,7 @@ namespace Гладиаторские_бои
     {
         private int _mana = 20;
 
-        public Priest() : base(110, 8, 33) { }
+        public Priest() : base("Жрец", 110, 8, 33) { }
 
         public override void TakeDamage(int damage)
         {
@@ -166,7 +168,7 @@ namespace Гладиаторские_бои
     {
         private int _mana = 20;
 
-        public Warlock() : base(105, 8, 35) { }
+        public Warlock() : base("Чернокнижник", 105, 8, 35) { }
 
         public override int DealDamage()
         {
@@ -200,7 +202,7 @@ namespace Гладиаторские_бои
         private int _mana = 20;
         private int _iceArmor = 0;
 
-        public Magician() : base(100, 8, 40) { }
+        public Magician() : base("Маг", 100, 8, 40) { }
 
         public override void TakeDamage(int damage)
         {
@@ -289,20 +291,17 @@ namespace Гладиаторские_бои
             if (fighter1.Health == 0 && fighter2.Health == 0)
             {
                 Console.WriteLine("Ничья");
-                Console.ReadKey();
+            }
+            else if (fighter1.Health > 0)
+            {
+                Console.WriteLine("Игрок 1 победил");
+            }
+            else if (fighter2.Health > 0)
+            {
+                Console.WriteLine("Игрок 2 победил");
             }
 
-            else if (fighter1.Health == 0)
-            {
-                Console.WriteLine("Игрок 1 проиграл");
-                Console.ReadKey();
-            }
-
-            else if (fighter2.Health == 0)
-            {
-                Console.WriteLine("Игрок 2 проиграл");
-                Console.ReadKey();
-            }
+            Console.ReadKey();
         }
 
         private void StartBattle(Fighter fighter1, Fighter fighter2)
@@ -319,49 +318,47 @@ namespace Гладиаторские_бои
 
                 Console.WriteLine();
                 ShowCombatLog(fighter1, fighter2);
-                Thread.Sleep(2000);
-                Console.Clear();
+                Console.ReadKey();
             }
         }
 
         private Fighter CreateWarrior()
         {
-            const string CommandWarrior = "1";
-            const string CommandDeathKnight = "2";
-            const string CommandPriest = "3";
-            const string CommandWarlock = "4";
-            const string CommandMagician = "5";
+            List<Fighter> fighters = new List<Fighter>() 
+            { 
+                new Warriour(),
+                new DeathKnight(),
+                new Priest(),
+                new Warlock(),
+                new Magician()
+            };
 
-            Console.WriteLine($"{CommandWarrior} - Воин");
-            Console.WriteLine($"{CommandDeathKnight} - Рыцарь Смерти");
-            Console.WriteLine($"{CommandPriest} - Жрец");
-            Console.WriteLine($"{CommandWarlock} - Чернокнижник");
-            Console.WriteLine($"{CommandMagician} - Маг");
-            string character = Console.ReadLine();
-
-            switch (character)
+            for (int i = 0; i < fighters.Count; i++)
             {
-                case CommandWarrior:
-                    return new Warriour();
-
-                case CommandDeathKnight:
-                    return new DeathKnight();
-
-                case CommandPriest:
-                    return new Priest();
-
-                case CommandWarlock:
-                    return new Warlock();
-
-                case CommandMagician:
-                    return new Magician();
-
-                default:
-                    Console.WriteLine("Такого Персонажа не существует");
-                    break;
+                Console.WriteLine($"{i + 1} - {fighters[i].Name}");
             }
 
-            return null;
+            int characterIndex = ReadNumber() - 1;
+
+            if ( characterIndex < 0 || characterIndex >= fighters.Count)
+            {
+                Console.WriteLine("Такого персонажа не существует");
+                return null;
+            }
+
+            return fighters[characterIndex];
+        }
+
+        private int ReadNumber()
+        {
+            int number;
+
+            while (int.TryParse(Console.ReadLine(), out number) == false)
+            {
+                Console.WriteLine("Ошибка. Введите число.");
+            }
+
+            return number;
         }
     }
 
